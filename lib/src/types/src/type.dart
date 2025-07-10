b68bc50988f1882f78626c9a6b400ff722f42063
Steve Hamblett <steve.hamblett@linux.com>
2025-07-10 15:21:01 +0100

    Add Types

---
+++ lib/src/types/src/type.dart
@@ -0,0 +1,8 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+part of '../types.dart';
---
+++ lib/src/types/types.dart
@@ -0,0 +1,10 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+library;
+
+part 'src/type.dart';
