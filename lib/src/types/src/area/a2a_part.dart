f18bea6dedaadff2e2b8b590549e4859af6a0b4f
Steve Hamblett <steve.hamblett@linux.com>
2025-07-12 11:09:20 +0100

    Type - Part types

--- lib/src/types/src/area/a2a_part.dart
+++ lib/src/types/src/area/a2a_part.dart
@@ -10,8 +10,64 @@ part of '../../types.dart';
 /// Represents a part of a message, which can be text, a file, or structured data.
 base class A2APart {}
 
-final class TextPart extends A2APart {}
+/// Represents a text segment within parts.
+final class TextPart extends A2APart {
+  /// Part type - text for TextParts
+  final kind = 'text';
 
-final class FilePart extends A2APart {}
+  /// Optional metadata associated with the part.
+  SV? metadata;
 
-final class DataPart extends A2APart {}
+  /// Text content
+  String text = '';
+}
+
+/// Represents a File segment within parts.
+final class FilePart extends A2APart {
+  /// Part type - file for TextParts
+  final kind = 'file';
+
+  /// Optional metadata associated with the part.
+  SV? metadata;
+
+  /// File content either as url or bytes
+  FilePartVariant? file;
+}
+
+final class DataPart extends A2APart {
+  /// Structured data content
+  SV data = {};
+
+  /// Part type - data for DataParts
+  final kind = 'data';
+
+  /// Optional metadata associated with the part.
+  SV? metadata;
+}
+
+/// File type variants
+base class FilePartVariant {}
+
+/// Define the variant where 'bytes' is present and 'uri' is absent
+final class FileWithBytes extends FilePartVariant {
+  /// base64 encoded content of the file
+  String bytes = '';
+
+  /// Optional mimeType for the file
+  String mimeTYpe = '';
+
+  /// Optional name for the file
+  String name = '';
+}
+
+/// Define the variant where 'uri' is present and 'bytes' is absent
+final class FileWithUrl extends FilePartVariant {
+  /// Optional mimeType for the file
+  String mimeTYpe = '';
+
+  /// Optional name for the file
+  String name = '';
+
+  /// URL for the File content
+  String url = '';
+}
