f46315c3a2c4d0bec688d60830a7552805e06178
Steve Hamblett <steve.hamblett@linux.com>
2025-07-14 11:28:54 +0100

    Type - Task Push Notification

--- lib/src/types/src/area/a2a_set_task_push_notification_config_response.dart
+++ lib/src/types/src/area/a2a_set_task_push_notification_config_response.dart
@@ -14,5 +14,14 @@ final class A2AJSONRPCErrorResponseSTPR
     extends A2ASetTaskPushNotificationConfigResponse
     with A2AJSONRPCErrorResponseM {}
 
+/// JSON-RPC success response model for the 'tasks/pushNotificationConfig/set' method.
 final class A2ASetTaskPushNotificationConfigSuccessResponseSTPR
-    extends A2ASetTaskPushNotificationConfigResponse {}
+    extends A2ASetTaskPushNotificationConfigResponse {
+  /// An identifier established by the Client that MUST contain a String, Number.
+  /// Numbers SHOULD NOT contain fractional parts.
+  A2AId? id;
+
+  /// Specifies the version of the JSON-RPC protocol. MUST be exactly "2.0".
+  final jsonrpc = '2.0';
+  A2ATaskPushNotificationConfig? result;
+}
