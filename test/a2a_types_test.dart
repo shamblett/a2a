8d955b7f6c20d4673966c4e6813912a97660cf9a
Steve Hamblett <steve.hamblett@linux.com>
2025-07-17 09:13:17 +0100

    Type - type tests

---
+++ test/a2a_types_test.dart
@@ -0,0 +1,15 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+@TestOn('vm')
+library;
+
+import 'package:test/test.dart';
+
+import 'package:a2a/a2a.dart';
+
+void main() {}
