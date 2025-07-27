5893cc0f341088f854e20236cc4b619903e6aa27
Steve Hamblett <steve.hamblett@linux.com>
2025-07-27 11:23:00 +0100

    Client - Switch to test agents for testing

--- test/a2a_client_test.dart
+++ test/a2a_client_test.dart
@@ -8,66 +8,32 @@
 @TestOn('vm')
 library;
 
-import 'dart:io';
 import 'dart:convert';
 
-import 'package:stream_channel/stream_channel.dart';
 import 'package:test/test.dart';
-import 'package:shelf/shelf.dart';
-import 'package:json_rpc_2/json_rpc_2.dart' as rpc;
-import 'package:web_socket_channel/web_socket_channel.dart';
-import 'package:a2a/a2a.dart';
 
-import 'support/a2a_test_client_server.dart';
-import 'support/a2a_test_rpc_server.dart';
+import 'package:a2a/a2a.dart';
 
 Future<int> main() async {
   late A2AClient testClient;
-
-  // Start the client test server
-  print('Client Test:: starting.....');
-  final testClientServer = await A2ATestClientServer().start();
-
-  // Check it
-  final ok = await Utilities.checkServer();
-  if (!ok) {
-    print('Client Test - Server is not available, exiting');
-    exit(-1);
-  }
-
   // JSON encoder
   String jsonEncode(Object? data) => JsonEncoder.withIndent(' ').convert(data);
 
   group('Client', () {
     test('Construction', () async {
-      final testCard = A2AAgentCard()
-        ..description = 'The Description'
-        ..name = 'The Name'
-        ..url = 'http://localhost:8081'
-        ..version = '1.0';
-      final testCardJson = testCard.toJson();
-      Response handler(Request request) => Response(
-        200,
-        headers: {
-          'content-type': 'application/json',
-          'Cache-Control': 'no-store',
-        },
-        body: jsonEncode(testCardJson),
-      );
       const baseUrl = 'http://localhost:8080/';
-      testClientServer.router.get('/.well-known/agent.json', handler);
       testClient = A2AClient(baseUrl);
       await Future.delayed(Duration(seconds: 1));
       expect(testClient.agentBaseUrl, 'http://localhost:8080');
-      expect(await testClient.serviceEndpoint, 'http://localhost:8081');
+      expect(await testClient.serviceEndpoint, 'http://localhost:8080');
     });
   });
 
   group('Agent', () {
     test('Get Agent Card', () async {
-      final agentCard = await testClient.getAgentCard();
+      final dynamic agentCard = {}; // TODO
       expect(testClient.agentBaseUrl, 'http://localhost:8080');
-      expect(await testClient.serviceEndpoint, 'http://localhost:8081');
+      expect(await testClient.serviceEndpoint, 'http://localhost:8080');
       expect(agentCard.agentProvider, isNull);
       expect(agentCard.description, 'The Description');
       expect(agentCard.name, 'The Name');
@@ -75,33 +41,19 @@ Future<int> main() async {
     });
     test('Get Service Endpoint', () async {
       final endpoint = await testClient.serviceEndpoint;
-      expect(endpoint, 'http://localhost:8081');
+      expect(endpoint, 'http://localhost:8080');
     });
   });
 
   group('Client RPC', () {
     test('Send Message', () async {
-      final rpcServer = A2ATestRPCServer();
-      void handler(HttpRequest request) {
-        final server = rpc.Server(StreamChannel<String>());
-        server.registerMethod('message/send', (rpc.Parameters params) {
-          print(params);
-        });
-        server.listen();
-      }
       try {
-        await rpcServer.start(handler);
-        await Future.delayed(Duration(seconds: 1));
-        final params = A2AMessageSendParams()
-          ..metadata = {'First': 1};
+        final params = A2AMessageSendParams()..metadata = {'First': 1};
         final response = await testClient.sendMessage(params);
         expect(response.isError, isFalse);
-      } catch(e) {
+      } catch (e) {
         rethrow;
-        rpcServer.stop();
       }
-
-      //rpcServer.stop();
     });
   });
   return 0;
