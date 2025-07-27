56b7801b7800674d3a80e6bcdcb6fcd1434cc860
Steve Hamblett <steve.hamblett@linux.com>
2025-07-27 13:57:36 +0100

    Client - Remote tests

---
+++ test/a2a_client_remote_test.dart
@@ -0,0 +1,62 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+@TestOn('vm')
+library;
+
+import 'dart:convert';
+
+import 'package:test/test.dart';
+
+import 'package:a2a/a2a.dart';
+
+/// Tests the client against the remote agent located at
+/// https://sample-a2a-agent-908687846511.us-central1.run.app`
+/// See the README file in test/support/hosts/cli for more details.
+Future<int> main() async {
+  late A2AClient testClient;
+
+  // JSON encoder
+  String jsonEncode(Object? data) => JsonEncoder.withIndent(' ').convert(data);
+
+  group('Client', () {
+    test('Construction', () async {
+      const baseUrl =
+          'https://sample-a2a-agent-908687846511.us-central1.run.app';
+      testClient = A2AClient(baseUrl);
+      await Future.delayed(Duration(seconds: 1));
+      expect(
+        testClient.agentBaseUrl,
+        'https://sample-a2a-agent-908687846511.us-central1.run.app',
+      );
+      expect(await testClient.serviceEndpoint, 'http://localhost:8080');
+    });
+  });
+  test('Get Agent Card', () async {
+    final dynamic agentCard = {}; // TODO
+    expect(testClient.agentBaseUrl, 'http://localhost:8080');
+    expect(await testClient.serviceEndpoint, 'http://localhost:8080');
+    expect(agentCard.agentProvider, isNull);
+    expect(agentCard.description, 'The Description');
+    expect(agentCard.name, 'The Name');
+    expect(agentCard.version, '1.0');
+  });
+  test('Get Service Endpoint', () async {
+    final endpoint = await testClient.serviceEndpoint;
+    expect(endpoint, 'http://localhost:8080');
+  });
+  test('Send Message', () async {
+    try {
+      final params = A2AMessageSendParams()..metadata = {'First': 1};
+      final response = await testClient.sendMessage(params);
+      expect(response.isError, isFalse);
+    } catch (e) {
+      rethrow;
+    }
+  });
+  return 0;
+}
