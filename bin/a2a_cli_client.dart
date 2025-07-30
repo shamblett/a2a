633787f0046a1330ac05f3a46b8012a29b13c659
Steve Hamblett <steve.hamblett@linux.com>
2025-07-30 20:43:10 +0100

    Framework

---
+++ bin/a2a_cli_client.dart
@@ -0,0 +1,16 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:colorize/colorize.dart';
+import 'package:args/args.dart';
+import 'package:uuid/uuid.dart';
+
+import 'package:a2a/a2a.dart';
