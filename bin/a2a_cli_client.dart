c460eee85159be90677005c45b9d15993ef8db08
Steve Hamblett <steve.hamblett@linux.com>
2025-07-31 09:35:47 +0100

    Cli client - implementation

--- bin/a2a_cli_client.dart
+++ bin/a2a_cli_client.dart
@@ -14,3 +14,45 @@ import 'package:args/args.dart';
 import 'package:uuid/uuid.dart';
 
 import 'package:a2a/a2a.dart';
+
+/// Simple cli client program for the A2AClient.
+///
+/// Usage : a2a_client_cli `<baseUrl>`
+/// If no baseUrl is supplied then https://sample-a2a-agent-908687846511.us-central1.run.app
+/// will be used,
+Future<int> main(List<String> argv) async {
+  // Parameters
+  String baseUrl;
+
+  ArgResults results;
+
+  // Initialize the argument parser
+  final argParser = ArgParser();
+  argParser.addFlag('help', abbr: 'h', negatable: false);
+
+  try {
+    results = argParser.parse(argv);
+  } on FormatException catch (e) {
+    print(e.message);
+    return -1;
+  }
+
+  // Help
+  if (results['help']) {
+    print('Usage: a2a_cli_client <baseUrl> of the agent');
+    print('');
+    print(argParser.usage);
+    return 0;
+  }
+
+  if (results.rest.isNotEmpty) {
+    baseUrl = results.rest.join('');
+  } else {
+    print('Usage:  a2a_cli_client <baseUrl> of the agent');
+    print('');
+    print(argParser.usage);
+    return 0;
+  }
+
+  return 0;
+}
