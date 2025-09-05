d565ba37b25387f3ad753769c7a0fce1572b5920
Steve Hamblett <steve.hamblett@linux.com>
2025-09-05 10:25:23 +0100

    Initial setup

---
+++ lib/src/mcp_bridge/a2a_mcp_bridge.dart
@@ -0,0 +1,15 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+library;
+
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:a2a/a2a.dart';
+import 'package:mcp_dart/mcp_dart.dart';
+
