2042aac274aa6d68b86fa1f4696266421e641abb
Steve Hamblett <steve.hamblett@linux.com>
2025-09-05 11:04:15 +0100

    Initial setup

--- lib/src/mcp_bridge/a2a_mcp_bridge.dart
+++ lib/src/mcp_bridge/a2a_mcp_bridge.dart
@@ -11,5 +11,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:a2a/a2a.dart';
+import 'package:colorize/colorize.dart';
 import 'package:mcp_dart/mcp_dart.dart';
 
+part 'src/a2a_mcp_client.dart';
