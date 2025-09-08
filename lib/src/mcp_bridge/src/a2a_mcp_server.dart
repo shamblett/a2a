ab786fa0955ddc2866c3fb259c062ab4f6dd7aff
Steve Hamblett <steve.hamblett@linux.com>
2025-09-08 09:40:07 +0100

    MCP Server

---
+++ lib/src/mcp_bridge/src/a2a_mcp_server.dart
@@ -0,0 +1,14 @@
+/*
+* Package : a2a
+* Author : S. Hamblett <steve.hamblett@linux.com>
+* Date   : 10/07/2025
+* Copyright :  S.Hamblett
+*/
+
+part of '../a2a_mcp_bridge.dart';
+
+///
+/// Provides an MCP server with a set of predefined tools for use
+/// by the [A2AMCPBridge] class.
+///
+class A2AMCPServer {}
