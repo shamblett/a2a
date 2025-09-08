e62c4f14ebd872bc8c4574cdbf3ac1fcc83ed76e
Steve Hamblett <steve.hamblett@linux.com>
2025-09-08 10:51:06 +0100

    MCP Server

--- lib/src/mcp_bridge/src/a2a_mcp_server.dart
+++ lib/src/mcp_bridge/src/a2a_mcp_server.dart
@@ -11,4 +11,32 @@ part of '../a2a_mcp_bridge.dart';
 /// Provides an MCP server with a set of predefined tools for use
 /// by the [A2AMCPBridge] class.
 ///
-class A2AMCPServer {}
+class A2AMCPServer {
+  static const serverName = 'A2A MCP Bridge Server';
+  static const serverVersion = '1.0.0';
+
+  static final _implementation = Implementation(
+    name: serverName,
+    version: serverVersion,
+  );
+
+  McpServer _server = McpServer(_implementation); // Default
+
+  final List<Tool> _tools = [];
+
+  A2AMCPServer() {
+    final serverCapabilities = ServerCapabilities(
+      tools: ServerCapabilitiesTools(listChanged: false),
+    );
+    final serverOptions = ServerOptions(
+      capabilities: serverCapabilities,
+      instructions: 'For use ony by the A2A MCP Bridge',
+    );
+    _server = McpServer(_implementation, options: serverOptions);
+    _initialiseTools();
+  }
+
+  void _initialiseTools() {
+
+  }
+}
