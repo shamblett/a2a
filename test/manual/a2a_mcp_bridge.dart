f3f4b8a3c662b3dee5991b784134181bf558863e
Steve Hamblett <steve.hamblett@linux.com>
2025-09-12 15:31:45 +0100

    MCP Bridge

--- test/manual/a2a_mcp_bridge.dart
+++ test/manual/a2a_mcp_bridge.dart
@@ -3,8 +3,45 @@ library;
 
 import 'package:test/test.dart';
 
-import 'package:a2a/a2a.dart';
+import 'package:mcp_dart/mcp_dart.dart';
+
+class AuthProvider implements OAuthClientProvider {
+  /// Get current tokens if available
+  @override
+  Future<OAuthTokens?> tokens() async =>
+      OAuthTokens(accessToken: '', refreshToken: '');
+
+  /// Redirect to authorization endpoint
+  @override
+  Future<void> redirectToAuthorization() async {
+    return;
+  }
+}
 
 Future<void> main() async {
+  final implementation = Implementation(
+    name: 'A2A MCP Bridge Manual Test',
+    version: '1.0.0',
+  );
+  final options = ClientOptions();
+  final client = Client(implementation, options: options);
 
+  final serverUrl = Uri.parse('http://localhost:3000');
+  final serverOptions = StreamableHttpClientTransportOptions(
+    //sessionId: 'A2A-MCP-Query',
+    authProvider: AuthProvider(),
+  );
+  final clientTransport = StreamableHttpClientTransport(
+    serverUrl,
+    opts: serverOptions,
+  );
+
+  test('Server ', () async {
+    await client.connect(clientTransport);
+    final serverVersion = client.getServerVersion();
+    expect(serverVersion, isNotNull);
+    expect(serverVersion?.name, 'A2A MCP Bridge Server');
+    expect(serverVersion?.version, '1.0.0');
+    await client.close();
+  });
 }
